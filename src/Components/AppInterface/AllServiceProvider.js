4ba66731922b0e411032ad65a9ec72a0da7d2a8a
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-10-05 00:54:24 +0600

    Added all services

--- src/Components/AppInterface/AllServiceProvider.js
+++ src/Components/AppInterface/AllServiceProvider.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Cleaning from './Cleaning';
+import Dance from './Dance';
+import Education from './Education';
+import Health from './Health';
+import HouseMaintain from './HouseMaintain';
+import Tour from './Tour';
 
 const AllServiceProvider = () => {
     return (
@@ -8,6 +13,11 @@ const AllServiceProvider = () => {
             <h1 className='text-center font-semibold text-[#388387] bg-[#c1f6f9] mx-2 mt-3'>All Popular Service Providers</h1>
 
             <Cleaning />
+            <HouseMaintain />
+            <Tour />
+            <Dance />
+            <Health />
+            <Education />
 
         </section>
     );
--- src/Components/AppInterface/Tour.js
+++ src/Components/AppInterface/Tour.js
@@ -1,36 +1,38 @@
 import React from 'react';
 
-const Cleaning = () => {
+const Tour = () => {
     return (
-        <div>
-            <h2 className='text-center font-semibold text-gray-500 mt-3'>Cleaning and Household Needs</h2>
-            <div className='border-t-2 w-3/4 border-gray-500 mx-auto'></div>
+        <section className='mt-4'>
+
+            <h2 className='text-center font-semibold text-gray-500 mt-3'>Tours and Travels</h2>
+            <div className='border-t-2 w-3/5 border-gray-500 mx-auto'></div>
 
             <div className='px-2 mt-3 flex overflow-x-auto'>
                 <div className='bg-white rounded mx-1 p-1'>
                     <img className='rounded' src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png" alt="" />
-                    <p className='text-[9px] text-center mt-1'>Cleaning Kits</p>
+                    <p className='text-[9px] text-center mt-2'>Bharat Taxi</p>
                 </div>
                 <div className='bg-white rounded mx-1 p-1'>
                     <img className='rounded' src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png" alt="" />
-                    <p className='text-[9px] text-center mt-1'>Cleaning Kits</p>
+                    <p className='text-[9px] text-center mt-2'>Bharat Taxi</p>
                 </div>
                 <div className='bg-white rounded mx-1 p-1'>
                     <img className='rounded' src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png" alt="" />
-                    <p className='text-[9px] text-center mt-1'>Cleaning Kits</p>
+                    <p className='text-[9px] text-center mt-2'>Bharat Taxi</p>
                 </div>
                 <div className='bg-white rounded mx-1 p-1'>
                     <img className='rounded' src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png" alt="" />
-                    <p className='text-[9px] text-center mt-1'>Cleaning Kits</p>
+                    <p className='text-[9px] text-center mt-2'>Bharat Taxi</p>
                 </div>
                 <div className='bg-white rounded mx-1 p-1'>
                     <img className='rounded' src="https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png" alt="" />
-                    <p className='text-[9px] text-center mt-1'>Cleaning Kits</p>
+                    <p className='text-[9px] text-center mt-2'>Bharat Taxi</p>
                 </div>
 
             </div>
-        </div>
+
+        </section>
     );
 };
 
-export default Cleaning;
+export default Tour;
