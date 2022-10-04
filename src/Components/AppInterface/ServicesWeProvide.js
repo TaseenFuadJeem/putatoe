0bdde3ba40f7fc2ef4c2572aec4c9b2713a5baeb
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-10-05 01:43:22 +0600

    Added img

--- src/Components/AppInterface/ServicesWeProvide.js
+++ src/Components/AppInterface/ServicesWeProvide.js
@@ -9,52 +9,52 @@ const ServicesWeProvide = () => {
             <div className='grid grid-cols-2 px-2 gap-3'>
                 <div>
                     <p className='text-xs bg-green-400 inline-block text-white p-1 relative top-6 left-[91.5px]'>20% OFF</p>
-                    <img src="http://koreabizwire.com/wp/wp-content/uploads/2019/08/fat-can.jpg" alt="" />
+                    <img src="https://i.ibb.co/RpswLvy/fat-can.jpg" alt="" />
                     <p className='text-center font-bold'>Daily Needs</p>
                 </div>
                 <div>
                     <p className='text-xs bg-green-400 inline-block text-white p-1 relative top-6 left-[91.5px]'>20% OFF</p>
-                    <img src="http://koreabizwire.com/wp/wp-content/uploads/2019/08/fat-can.jpg" alt="" />
+                    <img src="https://i.ibb.co/RpswLvy/fat-can.jpg" alt="" />
                     <p className='text-center font-bold'>Daily Needs</p>
                 </div>
                 <div>
                     <p className='text-xs bg-green-400 inline-block text-white p-1 relative top-6 left-[91.5px]'>20% OFF</p>
-                    <img src="http://koreabizwire.com/wp/wp-content/uploads/2019/08/fat-can.jpg" alt="" />
+                    <img src="https://i.ibb.co/RpswLvy/fat-can.jpg" alt="" />
                     <p className='text-center font-bold'>Daily Needs</p>
                 </div>
                 <div>
                     <p className='text-xs bg-green-400 inline-block text-white p-1 relative top-6 left-[91.5px]'>20% OFF</p>
-                    <img src="http://koreabizwire.com/wp/wp-content/uploads/2019/08/fat-can.jpg" alt="" />
+                    <img src="https://i.ibb.co/RpswLvy/fat-can.jpg" alt="" />
                     <p className='text-center font-bold'>Daily Needs</p>
                 </div>
                 <div>
                     <p className='text-xs bg-green-400 inline-block text-white p-1 relative top-6 left-[91.5px]'>20% OFF</p>
-                    <img src="http://koreabizwire.com/wp/wp-content/uploads/2019/08/fat-can.jpg" alt="" />
+                    <img src="https://i.ibb.co/RpswLvy/fat-can.jpg" alt="" />
                     <p className='text-center font-bold'>Daily Needs</p>
                 </div>
                 <div>
                     <p className='text-xs bg-green-400 inline-block text-white p-1 relative top-6 left-[91.5px]'>20% OFF</p>
-                    <img src="http://koreabizwire.com/wp/wp-content/uploads/2019/08/fat-can.jpg" alt="" />
+                    <img src="https://i.ibb.co/RpswLvy/fat-can.jpg" alt="" />
                     <p className='text-center font-bold'>Daily Needs</p>
                 </div>
                 <div>
                     <p className='text-xs bg-green-400 inline-block text-white p-1 relative top-6 left-[91.5px]'>20% OFF</p>
-                    <img src="http://koreabizwire.com/wp/wp-content/uploads/2019/08/fat-can.jpg" alt="" />
+                    <img src="https://i.ibb.co/RpswLvy/fat-can.jpg" alt="" />
                     <p className='text-center font-bold'>Daily Needs</p>
                 </div>
                 <div>
                     <p className='text-xs bg-green-400 inline-block text-white p-1 relative top-6 left-[91.5px]'>20% OFF</p>
-                    <img src="http://koreabizwire.com/wp/wp-content/uploads/2019/08/fat-can.jpg" alt="" />
+                    <img src="https://i.ibb.co/RpswLvy/fat-can.jpg" alt="" />
                     <p className='text-center font-bold'>Daily Needs</p>
                 </div>
                 <div>
                     <p className='text-xs bg-green-400 inline-block text-white p-1 relative top-6 left-[91.5px]'>20% OFF</p>
-                    <img src="http://koreabizwire.com/wp/wp-content/uploads/2019/08/fat-can.jpg" alt="" />
+                    <img src="https://i.ibb.co/RpswLvy/fat-can.jpg" alt="" />
                     <p className='text-center font-bold'>Daily Needs</p>
                 </div>
                 <div>
                     <p className='text-xs bg-green-400 inline-block text-white p-1 relative top-6 left-[91.5px]'>20% OFF</p>
-                    <img src="http://koreabizwire.com/wp/wp-content/uploads/2019/08/fat-can.jpg" alt="" />
+                    <img src="https://i.ibb.co/RpswLvy/fat-can.jpg" alt="" />
                     <p className='text-center font-bold'>Daily Needs</p>
                 </div>
             </div>
